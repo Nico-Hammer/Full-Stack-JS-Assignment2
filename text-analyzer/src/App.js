bb02e4e464da7332ff67956c6314ed022dfe19f7
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-11-20 19:51:32 -0700

    added form component

--- text-analyzer/src/App.js
+++ text-analyzer/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
+import TextInput from './components/textInputForm';
 function App() {
   return (
-    <h1>Text analyzer</h1>
+    <div>
+      <h1>Text analyzer</h1>
+      <TextInput/>
+    </div>
   );
 }
 export default App;
---
+++ text-analyzer/src/components/textInputForm.js
@@ -0,0 +1,12 @@
+function TextInput(){
+    return(
+        <div>
+            <form>
+                <input type="text" placeholder="Enter some text"/>
+                <br/>
+                <button type="submit">Analyze</button>
+            </form>
+        </div>
+    );
+}
+export default TextInput;
